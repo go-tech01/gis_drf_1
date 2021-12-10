23c05ab943d3d80b76daf17180cd0f8e3a733eca
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-10 13:57:28 +0900

    models list (database)

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,5 +10,8 @@ def hello_world(request):
         new_model = NewModel()
         new_model.text = input_data
         new_model.save()
-        return render(request, 'accountapp/hello_world.html', context={'new_model':new_model})
-    return render(request, 'accountapp/hello_world.html')
+        new_model_list = NewModel.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'new_model':new_model, 'new_model_list':new_model_list})
+    new_model_list = NewModel.objects.all()
+    return render(request, 'accountapp/hello_world.html', context={'new_model_list':new_model_list})
