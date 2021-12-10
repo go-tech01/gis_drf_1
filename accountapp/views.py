8f313d9efd0d9606d043c379e766c77909919921
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-10 11:42:46 +0900

    models

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from accountapp.models import NewModel
 
-# Create your views here.
 
 def hello_world(request):
     # return HttpResponse('Hello World!')
     if request.method == "POST":
         input_data = request.POST.get('input_data')
-        return render(request, 'accountapp/hello_world.html', context={'message':input_data})
+        new_model = NewModel()
+        new_model.text = input_data
+        new_model.save()
+        return render(request, 'accountapp/hello_world.html', context={'new_model':new_model})
     return render(request, 'accountapp/hello_world.html')
