0b9e21b72d7e9230d6d431a479d33d46ffff6571
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-13 15:37:54 +0900

    logout view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/',AccountCreateView.as_view(), name='create'),
 ]
