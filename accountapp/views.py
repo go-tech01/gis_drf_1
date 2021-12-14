99e372e8686e0bb7fcdc1c8d18d38a6c94d5f490
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 11:48:42 +0900

    account deleteview

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import NewModel
@@ -36,4 +36,11 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world') # 나중에 경로 변경
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
