1a05dd1a04c000de385fdfeace64f4339c7bda96
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 10:55:24 +0900

    Profileapp Created

--- gis_drf_1/settings.py
+++ gis_drf_1/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'accountapp'
+    'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
