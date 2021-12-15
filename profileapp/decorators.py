604cf07b7f640d2844c3641eb6a924a1247a9e3c
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 16:54:58 +0900

    Profile decortor

---
+++ profileapp/decorators.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponseForbidden
+from profileapp.models import Profile
+
+def profile_ownership_required(func):
+    def decorated(requset, *args, **kwargs):
+        target_profile = Profile.objects.get(kwargs['pk'])
+        if not target_profile.owner == requset.user:
+            return HttpResponseForbidden()
+        else:
+            return func(requset, *args, **kwargs)
+    return decorated
