edbbac347b125bb42422f1cca1255308a4ef89f0
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 11:57:52 +0900

    Profile Signals

--- profileapp/models.py
+++ profileapp/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -9,3 +12,8 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True, blank=True)
     message = models.CharField(max_length=255,null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+@receiver(post_save, sender=User)
+def create_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance, nickname='임시 닉네임')
