5d36de94ff736ee15f87cf9a1b48214146e99bdb
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 16:46:40 +0900

    Profile UpdateView

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -13,3 +13,14 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk':self.object.owner.pk})
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.owner.pk})
+    
+    # success_url = 
+    # get_success_url() 차이점?
