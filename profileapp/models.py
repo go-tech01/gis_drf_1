48286c280d6b169d5a0bbdebe0d2b65e3cb746a6
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 11:11:36 +0900

    Profileapp CreateView

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    nickname = models.CharField(max_length=50, null=False)
+    image = models.ImageField(upload_to='profile/', null=True, blank=True)
+    message = models.CharField(max_length=255,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
