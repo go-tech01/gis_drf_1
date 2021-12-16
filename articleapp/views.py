0ce120a83d31acb93df02a13929e3eaa9cd0e1da
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 17:19:11 +0900

    ArticleApp CreateView

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
-
 
 class ArticleCreateView(CreateView):
     model = Article
