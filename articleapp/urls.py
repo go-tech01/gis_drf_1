4345f5cc816bd0bac774e6ba6bfce7de537f5d3f
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 17:04:06 +0900

    ArticleApp create

---
+++ articleapp/urls.py
@@ -0,0 +1,8 @@
+# from django.urls import path
+# from django.views.generic import TemplateView
+#
+# app_name = 'articleapp'
+#
+# urlpatterns = [
+#     path('list/', TemplateView(template_name=))
+# ]
