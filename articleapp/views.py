37a323723e83b59c542c6c8bffe12a50c35166bd
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-17 15:34:39 +0900

    Articleapp UpdateView & DeleView, decorator(permission)

--- articleapp/views.py
+++ articleapp/views.py
@@ -25,16 +25,17 @@ class ArticleDetailView(DetailView):
     template_name = 'articleapp/detail.html'
 
 @method_decorator(article_ownership_required, 'get')
-@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
 class ArticleUpdateView(UpdateView):
     model = Article
+    form_class = ArticleCreationForm
     context_object_name = 'target_article'
     template_name = 'articleapp/update.html'
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
 
 @method_decorator(article_ownership_required, 'get')
-@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
 class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
